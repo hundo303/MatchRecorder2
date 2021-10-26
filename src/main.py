b716b76f970a407309485682556f44b5d6d4ad47
hundo303 <sakaigen303@gmail.com>
2021-10-26 19:21:15 +0900

    [update] オプションを受け取れるように変更

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 import crawling
 import write_db
 import datetime
+from argparse import ArgumentParser
+
 
 if __name__ == '__main__':
-    now_year = datetime.date.today().year
-    crawling.craw()
-    write_db.write_db(f'{now_year}', now_year)
+    arg_parser = ArgumentParser()
+
+    arg_parser.add_argument('-l', '--lite', action='store_true')
+    arg = arg_parser.parse_args()
+
+    if arg.lite:
+        pass
+    else:
+        now_year = datetime.date.today().year
+        crawling.craw()
+        write_db.write_db(f'{now_year}', now_year)
